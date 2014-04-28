a5c8479c9ad246cc14d0f5bcff1eff0c3bb7acf5
rsricha8 <rs3375@columbia.edu>
2014-04-28 04:30:18 -0400

    Delete TagInUser.R
    No more needed. See TagUserMapping.R

--- src/TagInUser.R
+++
@@ -1,6 +0,0 @@
-
-ds <- postsXMLDF[postsXMLDF$PostTypeId == '1',c("Id", "AcceptedAnswerId", "Tags")]
-QuestionsPost <- subset(postsXMLDF,select=c(PostTypeId=='1'))
-View(QuestionsPost)
-View(ds)
-nrow(ds)
