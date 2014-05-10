5bbb1af7bbfd24866c0834156703d16348e0aec8
Divi Khanna <dk2745@columbia.edu>
2014-05-10 00:23:19 -0400

    Minor

--- src/Tag Clusters.R
+++ src/Tag Clusters.R
@@ -9,7 +9,7 @@ t <- str_extract_all(tags, "(<)(.*?)(>)")
 t1 <- unique(unlist(t))
 t2 <- as.data.frame(table(unlist(t)))
 t2 <- t2[with(t2, order(-Freq)),]
-t2 <- head(t2,.1*length(t1))
+t2 <- head(t2,.5*length(t1))
 
 #Entire graph
 pairs <- data.frame()
@@ -20,25 +20,29 @@ graph <- graph.data.frame(matrix,directed=FALSE, vertices=t1)
 E(graph)
 V(graph)
 E(graph)$width <- count.multiple(graph)
-igraph.options(vertex.label=t1, vertex.shape="circle",vertex.size=1)
-plot.igraph(graph, layout=layout.circle, edge.curved=FALSE)
+igraph.options(vertex.label=NA, vertex.shape="circle",vertex.size=4)
+plot.igraph(graph, layout=layout.fruchterman.reingold, edge.curved=FALSE)
 wc <-  edge.betweenness.community(graph)
 modularity(wc)
 membership(wc)
 plot(wc, graph)
 plot(graph, vertex.color=membership(wc))
+large.clusters <- which(table(wc$membership)>=5)
+wc$membership <- wc$membership %in% large.clusters
+
 
 #100 random tags
 test <- matrix[sample(nrow(matrix), 100),]
 v <- matrix(rbind(test[,1],test[,2]),ncol=1)
 graph <- graph.data.frame(test,directed=FALSE, vertices=unique(v))
 E(graph)$width <- count.multiple(graph)
-igraph.options(vertex.label=NA,vertex.label.cex=0.9, vertex.shape="circle",vertex.size=1)
-plot.igraph(graph, layout=layout.circle, edge.curved=FALSE)
+igraph.options(vertex.label=NA, vertex.shape="circle",vertex.size=4)
+plot.igraph(graph, layout=layout.fruchterman.reingold, edge.curved=FALSE)
 wc <-  edge.betweenness.community(graph)
 modularity(wc)
 membership(wc)
 plot(wc, graph)
+
 
 #Top 20% tags
 require(scales)
@@ -57,7 +61,7 @@ radian.rescale <- function(x, start=0, direction=1) {
 }
 position <- radian.rescale(x=1:nrow(t2), direction=-1, start=0)
 plot.igraph(graph, layout=layout.circle,vertex.label.dist=1,vertex.label.degree=position, edge.curved=FALSE)
-wc <-  edge.betweenness.community(graph)
+wc <-  leading.eigenvector.community(graph)
 modularity(wc)
 membership(wc)
 plot(wc, graph)
